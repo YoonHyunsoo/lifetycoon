d37b7e71731a75b52cf62aab20756b1dd83cbb35
YoonHyunsoo <75830517+YoonHyunsoo@users.noreply.github.com>
2026-02-01 22:37:09 +0900

    Update: Fix bugs and update docs

--- force_push.bat
+++ force_push.bat
@@ -4,6 +4,8 @@ echo.
 echo WARNING: This will overwrite the repository at https://github.com/YoonHyunsoo/lifetycoon.git
 echo with the files currently on your computer.
 echo.
+git add .
+git commit -m "Update: Fix bugs and update docs"
 git push -u origin main --force
 echo.
 echo If you see "Everything up-to-date" or a success message, it worked!
